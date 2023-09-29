7dd82dde989f95f53bcbeddc02932c5f52e460d8
Rahul M <roboto7o32oo3@gmail.com>
2023-09-29 23:01:58 +0530

    Implement proper physics for ball jumping

--- player.py
+++ player.py
@@ -32,10 +32,13 @@ class Player(Entity):
 
         # JUMP PHYSICS VARIABLES
         self.gravity = 40 * GAME_SPEED
-        self.jump_height = 2 
+        self.jump_height = 3
+        self.air_time = 0
+        self.velocity = 0
+        self.prev_y = 0
 
     def update(self):
-        self.direction = Vec3(self.right * (held_keys['d'] - held_keys['a'])).normalized()
+        self.direction = Vec3(self.up * (held_keys['space']) + self.right * (held_keys['d'] - held_keys['a'])).normalized()
 
         feet_ray = raycast(self.position+Vec3(0,0.5,0), self.direction, traverse_target=self.traverse_target, ignore=self.ignore_list, distance=.5, debug=False)
         head_ray = raycast(self.position+Vec3(0,self.height-.1,0), self.direction, traverse_target=self.traverse_target, ignore=self.ignore_list, distance=.5, debug=False)
@@ -57,28 +60,33 @@ class Player(Entity):
         ray = raycast(self.world_position+(0,self.height,0), self.down, traverse_target=self.traverse_target, ignore=self.ignore_list)
         # ray = boxcast(self.world_position+(0,2,0), self.down, ignore=self.ignore_list)
         # if not on ground and not on way up in jump, fall
+        curr_y = self.prev_y + self.calc_position(self.velocity, self.air_time, self.gravity)
+        self.y = max(0, curr_y)
+
+        # print(self.calc_position(self.velocity, self.air_time, self.gravity))
+
+        if curr_y <= 0:
+            # print('at ground')
+            self.air_time = 0
+            self.velocity = 0
+            self.prev_y = 0
+        else:
+            self.air_time += time.dt
+
+    def calc_velocity(self, height, gravity):
+        return sqrt(2 * height * gravity)
+    
+    def calc_position(self, initial_velocity, time, gravity):
+        return (initial_velocity * time) - (gravity * time**2) / 2
 
     def input(self, key):
-        if key == 'w' and self.y <= 16:
+        if key == 'space' and self.y + self.height <= 16:
             self.jump()
-
-    # function to find the time taken for a ball to reach a given height 
-    # when thrown vertically upward under gravity g
-    def fall_duration(self, height):
-        return sqrt((2 * height) / self.gravity)
-
+    
     def jump(self):
-        if hasattr(self, 'y_animator'):
-            self.y_animator.pause()
-
-        duration = self.fall_duration(self.jump_height)
-        print(self.y)
-        self.animate_y(self.y+self.jump_height, duration, resolution=int(1//time.dt), curve=curve.out_quad, auto_destroy = True)
-        invoke(self.start_fall, delay=duration-0.1)
-
-    def start_fall(self):
-        self.y_animator.pause()
-        self.animate_y(0, self.fall_duration(self.y), resolution=int(1//time.dt), curve=curve.in_quad, auto_destroy = True)
+        self.velocity = self.calc_velocity(self.jump_height, self.gravity)
+        self.air_time = time.dt
+        self.prev_y = self.y
 
     def on_enable(self):
         mouse.locked = True
